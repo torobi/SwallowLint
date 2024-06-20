a0e539bea28a0e5af22877480bdef4ae042e0e3c
torobi <torobi2222@gmail.com>
2024-06-21 00:06:46 +0900

    Add config option

--- Sources/swallowlint/SwallowLint.swift
+++ Sources/swallowlint/SwallowLint.swift
@@ -1,7 +1,18 @@
+import Foundation
 import ArgumentParser
+import Yams
 
 @main
 struct SwallowLint: ParsableCommand {
+    @Option(
+        name: [.customLong("config")],
+        help: "Config file path.",
+        completion: .file(extensions: ["yml", "yaml"])
+    )
+    var configPath: String = ".swallowlint.yml"
+
+    private var config: Config?
+
     static let configuration: CommandConfiguration = .init(
         commandName: "swallow-lint",
         abstract: "simple swift linter.",
@@ -10,6 +21,21 @@ struct SwallowLint: ParsableCommand {
     )
 
     mutating func run() throws {
-        print("run")
+        config = try readConfig()
+        print(config)
+    }
+}
+
+// MARK: - Config
+private extension SwallowLint {
+    mutating func readConfig() throws -> Config {
+        guard FileManager.default.fileExists(atPath: configPath) else {
+            return Config()
+        }
+        let url = URL(fileURLWithPath: configPath)
+        let decoder = YAMLDecoder()
+
+        let data = try Data(contentsOf: url)
+        return try decoder.decode(Config.self, from: data)
     }
 }
